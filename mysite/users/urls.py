047cd3f8ae761411cfece484c6bf8f96e62eee06
Ярослав Осипов <Oyaroslav2001@gmail.com>
2024-09-26 01:32:58 +0500

    n

--- mysite/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from users.views import register, dashboard
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('dashboard/', dashboard, name='dashboard'),
-]
